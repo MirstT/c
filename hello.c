c67d75303f57ca1448519aeabfebd71d75064bd5
MirstT <W_H_Norway@outlook.com>
2021-11-18 22:57:50 +0800

    test2

--- hello.c
+++ hello.c
@@ -4,15 +4,16 @@
  * @Author       : Mirst
  * @Date         : 2021-11-18 22:32:36
  * @LastEditors  : Mirst
- * @LastEditTime : 2021-11-18 22:47:54
+ * @LastEditTime : 2021-11-18 22:55:10
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(){
+    int a=1;
+    int b=a;
+    printf("%d\n",b);
     printf("test");
-    system("pause");
     return 0;
-    return;
 }
