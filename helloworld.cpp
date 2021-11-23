991026702bc89bee81349b09f7df386b8eabf64e
Mirst <Mirst@outlook.com>
2021-11-23 11:26:53 +0800

    init

---
+++ helloworld.cpp
@@ -0,0 +1,23 @@
+/*
+ * @Description  :  
+ * @version      : 
+ * @Author       : Mirst
+ * @Date         : 2021-11-23 05:54:37
+ * @LastEditors  : Mirst
+ * @LastEditTime : 2021-11-23 11:05:32
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
