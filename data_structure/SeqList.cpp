d0d8f6e200203cd6de2ddf5937161aacd8aad2b1
Mirst <Mirst@outlook.com>
2021-11-26 11:41:07 +0800

    顺序表的静态实现更新

--- data_structure/SeqList.cpp
+++ data_structure/SeqList.cpp
@@ -1,8 +1,16 @@
 /*
- * @Description  : 
- * @version      : 
+ * @Description  : 顺序表的动态实现
+ * @version      : 1.0.0
  * @Author       : Mirst
  * @Date         : 2021-11-25 14:17:57
  * @LastEditors  : Mirst
  * @LastEditTime : 2021-11-25 14:17:57
  */
+
+typedef struct SeqList
+{
+    int *data;
+    int length;
+    S *next;
+}S;
+
